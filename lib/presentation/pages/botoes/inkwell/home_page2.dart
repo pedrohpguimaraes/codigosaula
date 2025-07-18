4d088cd01e70a256b048fc15113b840ef7fbbd48
pedro1.guimaraes <pedro1.guimaraes@alunos.ifsuldeminas.edu.br>
2025-07-18 11:54:00 +0000

    widget1807

---
+++ lib/presentation/pages/botoes/inkwell/home_page2.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const HomePage());
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'InkWell - Exemplo 1',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const InkWellExampleHome(),
+    );
+  }
+}
+
+class InkWellExampleHome extends StatefulWidget {
+  const InkWellExampleHome({super.key});
+
+  @override
+  State<InkWellExampleHome> createState() => _InkWellExampleHomeState();
+}
+
+class _InkWellExampleHomeState extends State<InkWellExampleHome> {
+  double boxSize = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('InkWell com Animação'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          width: boxSize,
+          height: boxSize,
+          child: Material(
+            color: Colors.deepPurple,
+            borderRadius: BorderRadius.circular(12),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(12),
+              splashColor: Colors.white24,
+              onTap: () {
+                setState(() {
+                  boxSize = boxSize == 100 ? 150 : 100;
+                });
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('InkWell ativado!')),
+                );
+              },
+              child: const Center(
+                child: Text(
+                  'Toque aqui',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
